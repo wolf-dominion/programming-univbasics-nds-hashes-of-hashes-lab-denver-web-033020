5e9f4b9276145c4aac71074a7db49e6db59a019f
Hannah Drury <hannah_drury@yahoo.com>
2020-01-21 22:58:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,7 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
+  
+  hoh = {:PORTION_1 => {:PORTION_2 => :PORTION_3 =>{PORTION_4}}}
 
 end
